09017b869f31bd2cb84d779d50a5689861b65d14
Hankyu2000 <yuduki.developer@gmail.com>
2022-06-12 20:08:45 +0900

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+//document.addEventListener('DOMContentLoaded', () => {
     const date = new Date('June 2, 2022 00:00:00')
     const dateNow = Date.now();
     const diff = dateNow - date;
-
-});
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    //document.getElementById("days").textContent = diff.toLocaleString('ja-jp', options)
+//});
